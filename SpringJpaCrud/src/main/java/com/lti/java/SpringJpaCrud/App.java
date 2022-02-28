93d21f4001355518e54f40d5224e46e4c7006846
madhurighatage <81734433+madhurighatage@users.noreply.github.com>
2022-02-28 15:15:45 +0530

    Update App.java

--- SpringJpaCrud/src/main/java/com/lti/java/SpringJpaCrud/App.java
+++ SpringJpaCrud/src/main/java/com/lti/java/SpringJpaCrud/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Java Hello World! Welcome all" );
+        System.out.println( "Hello World! Welcome all" );
     }
 }
